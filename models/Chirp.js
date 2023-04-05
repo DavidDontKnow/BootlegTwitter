b752991c7b51e7c4f514c108bffa964c71c81458
chrisgom113 <cgomez4@uci.edu>
2023-04-04 19:39:10 -0700

    added Chirp model, added associations in index.js

--- models/Chirp.js
+++ models/Chirp.js
@@ -4,32 +4,32 @@ const sequelize = require('../config/connection.js');
 class Chirp extends Model {}
 
 Chirp.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-          },
-        content: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },  
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-              model: 'user',
-              key: 'id',
-            },
-          },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'chirp',
-      }
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'chirp',
+  }
 );
 
 module.exports = Chirp;
