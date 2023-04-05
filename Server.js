6b48280b03c0f8494566a1db7a92fd5585ce1ec5
UserHAL-9000 <jonconnorrobinson94@gmail.com>
2023-04-04 20:27:07 -0700

    server session

--- Server.js
+++ Server.js
@@ -24,6 +24,7 @@ const sess = {
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app,use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
