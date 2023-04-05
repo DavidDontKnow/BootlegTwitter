2b4cb28685314e01a75aea36e32932674f454eb8
Jazmine Garcia <jazminegarcia2015@gmail.com>
2023-04-05 15:46:45 -0700

    adds login code

---
+++ .history/config/connection_20230405153934.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+require('dotenv').config(); // Load environment variables from .env file
+
+let sequelize; // Initialize Sequelize
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3000
+        }
+    );
+}
+
+// Please remember to have a .env file in your root directory with the following variables:\r\nDB_NAME=your_db_name\r\nDB_USER=your_db_user\r\nDB_PASSWORD=your_db_password
+
+
+module.exports = sequelize;
