d1b9d884ecc9bc93b52c65504bdcf27d27ce1601
Harrison <harrylamm@icloud.com>
2023-04-04 20:56:23 -0700

    fixed server.js

--- Server.js
+++ Server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
 const sess = {
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
